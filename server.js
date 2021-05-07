c9b7eb50b0caca1d605cd134955b94655380e817
Max Zhang <maxzhang@max-zhang-macbook.local>
2021-05-06 23:54:46 -0700

    public script.js fix

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const { v4: uuid } = require('uuid')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
+    res.redirect(`/${uuid()}`)
 })
 
 app.get('/:room', (req, res) => {
